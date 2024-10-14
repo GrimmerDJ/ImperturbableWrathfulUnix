e89f55b6a753f87d80c26588cc9f2c5019015cac
GrimmerDJ <dgrimmer.exe@gmail.com>
2024-10-14 01:53:01 +0000

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        double scam = 10000;
-        double scamRate = 0.05;
+        double tuition = 10000;
+        double tuitionGrowthRate = 0.05;
 
         for (int year = 1; year <= 10; year++) {
-            scam += scam * scamRate;
+            tuition += tuition * tuitionGrowthRate;
         }
-        System.out.printf("Tuition in ten years: $%.2f%n", scam);
+        System.out.printf("Tuition in ten years: $%.2f%n", tuition);
 
         // I'm just going to assume that you mean for me to calculate tuition starting from year 10 and not 7
         double totalScam = 0;
         for (int year = 1; year <= 4; year++) {
-            totalScam += scam;
-            scam += scam * scamRate;
+            totalScam += tuition;
+            tuition += tuition * tuitionGrowthRate;
         }
         System.out.printf("Total cost for four years after ten years: $%.2f%n", totalScam);
     }
